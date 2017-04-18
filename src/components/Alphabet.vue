0a3f745e2c48b44a3e73ac14c82275e7b9c1f15d
D'ontreye Nero <dontreyenero@gmail.com>
2017-04-18 06:16:54 -0400

    Initial commit

---
+++ src/components/Alphabet.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+  	<div>
+	  Welcome to Learn With Me
+	</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+</style>
